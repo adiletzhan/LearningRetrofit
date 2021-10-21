6eff8e8ec321d2639799b29529ed7454b6e75ca8
adiletzhan <adiletzhan.kambetov@gmail.com>
2021-10-21 17:55:39 +0600

    Initial commit

---
+++ app/src/main/java/com/example/learningretrofit/TodoApi.kt
@@ -0,0 +1,9 @@
+package com.example.learningretrofit
+
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface TodoApi {
+    @GET("/todos")
+    suspend fun getTodos(): Response<List<Todo>>
+}
